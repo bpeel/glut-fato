8df6da87b128b2cfc78c28e2be0d00966443ebc8
Neil Roberts <neil@linux.intel.com>
2014-02-26 17:48:29 +0000

    Initial triangle example

---
+++ src/fato-triangle.c
@@ -0,0 +1,33 @@
+#include "config.h"
+
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+static void
+display_cb(void)
+{
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glColor3f(1.0f, 0.0f, 0.0f);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-1.0f, -1.0f);
+        glVertex2f(1.0f, -1.0f);
+        glVertex2f(0.0f, 1.0f);
+        glEnd();
+
+        glutSwapBuffers();
+}
+
+int
+main(int argc, char **argv)
+{
+        glutInit(&argc, argv);
+        glutCreateWindow("Triangle");
+        glutDisplayFunc(display_cb);
+
+        glutMainLoop();
+
+        return EXIT_SUCCESS;
+}
