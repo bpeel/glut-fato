340db51545fd542ce6949c3781a28589d7719857
Neil Roberts <neil@linux.intel.com>
2014-03-18 16:00:54 +0000

    pbo-thing: Dump out the image

--- src/fato-pbo.c
+++ src/fato-pbo.c
@@ -2,13 +2,16 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
 static GLuint tex;
 
-#define TEX_SIZE 256
-#define WINDOW_SIZE TEX_SIZE
+#define TEX_WIDTH 32
+#define TEX_HEIGHT 128
+#define WINDOW_WIDTH TEX_WIDTH
+#define WINDOW_HEIGHT TEX_HEIGHT
 
 static GLuint
 create_tex(void)
@@ -16,7 +19,7 @@ create_tex(void)
         GLuint tex;
         GLuint buf;
         uint8_t *data;
-        int y, x;
+        int i;
 
         glGenTextures(1, &tex);
         glBindTexture(GL_TEXTURE_2D, tex);
@@ -28,18 +31,16 @@ create_tex(void)
         glGenBuffers(1, &buf);
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, buf);
         glBufferData(GL_PIXEL_UNPACK_BUFFER,
-                     TEX_SIZE * TEX_SIZE * 4,
+                     TEX_WIDTH * TEX_HEIGHT * 4,
                      NULL,
                      GL_STATIC_DRAW);
         data = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
 
-        for (y = 0; y < TEX_SIZE; y++) {
-                for (x = 0; x < TEX_SIZE; x++) {
-                        *(data++) = x;
-                        *(data++) = y;
-                        *(data++) = 0;
-                        *(data++) = 255;
-                }
+        for (i = 0; i < TEX_WIDTH * TEX_HEIGHT; i++) {
+                *(data++) = 0;
+                *(data++) = i >> 8;
+                *(data++) = i & 0xff;
+                *(data++) = 255;
         }
 
         glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
@@ -47,9 +48,9 @@ create_tex(void)
         glTexImage2D(GL_TEXTURE_2D,
                      0, /* level */
                      GL_RGBA,
-                     TEX_SIZE, TEX_SIZE,
+                     TEX_WIDTH, TEX_HEIGHT,
                      0, /* border */
-                     GL_RGBA,
+                     GL_BGRA,
                      GL_UNSIGNED_BYTE,
                      NULL /* data */);
 
@@ -59,6 +60,27 @@ create_tex(void)
         glBindTexture(GL_TEXTURE_2D, 0);
 
         return tex;
+}
+
+static void
+dump_image(void)
+{
+        uint8_t buf[WINDOW_WIDTH * 4 * WINDOW_HEIGHT], *p = buf;
+        int y, x;
+
+        glReadPixels(0, 0, /* x/y */
+                     WINDOW_WIDTH, WINDOW_HEIGHT,
+                     GL_RGBA,
+                     GL_UNSIGNED_BYTE,
+                     buf);
+
+        for (y = 0; y < WINDOW_HEIGHT; y++) {
+                for (x = 0; x < WINDOW_WIDTH; x++) {
+                        printf("%04x ", p[0] | (p[1] << 8));
+                        p += 4;
+                }
+                fputc('\n', stdout);
+        }
 }
 
 static void
@@ -85,14 +107,18 @@ display_cb(void)
         glDisable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, 0);
 
+        dump_image();
+
         glutSwapBuffers();
+
+        glutLeaveMainLoop();
 }
 
 int
 main(int argc, char **argv)
 {
         glutInit(&argc, argv);
-        glutInitWindowSize(WINDOW_SIZE, WINDOW_SIZE);
+        glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         glutCreateWindow("Look! A triangle!");
         glutDisplayFunc(display_cb);
         glewInit();
